80ee7789794e1bc53a1c97849f6ab1cd3b5acbc9
five-three <zjdwsxy@163.com>
2023-12-16 15:07:00 +0800

    更新

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,34 +6,18 @@ module.exports = {
     // 站点配置, 设置为 /[仓库名]/
     base: '/',
     head: [
-        // ...
         ['style', {}, `
-        html {
-          height: 100%;
-        }
-      
         body {
-          margin: 0;
-          overflow: hidden; /* 禁止body的滚动条 */
-          height: 100%;
-          display: flex;
-          flex-direction: column;
-        }
-      
-        .background-container {
           background-image: url('/bg.png');
           background-repeat: no-repeat;
           background-attachment: fixed;
           background-size: cover;
-          flex: 1;
-          overflow-y: auto; /* 允许滚动 */
+          margin: 0;
+          height: 100vh;
+          display: flex;
+          align-items: center;
+          justify-content: center;
         }
-      
-        /* 添加其他页面内容样式，确保内容不被背景遮挡 */
-        .content {
-          padding: 20px;
-          /* 其他内容样式 */
-        }
-      `]      
+      `]
     ],
 }
